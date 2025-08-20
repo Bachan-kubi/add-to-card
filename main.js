ad191e7ac5b1c5b9bbee32f65432266128ae4fee
Bachan Kubi <felixghagra@gmail.com>
2025-08-20 18:18:36 +0600

    product design started

--- main.js
+++ main.js
@@ -8,19 +8,21 @@ if(productContainer){
 //2- display product 
 function displayProducts(){
     products.forEach(product => {
+        console.log(product);
         const productCard = document.createElement('div');
         productCard.classList.add('product-card');
         productCard.innerHTML = `
             <div class="img-box">
                 <img src="${product.colors[0].mainImage}" alt="${product.title}" />
             </div>
-            <h2 class="title>${product.title}</h2>
-            <p>${product.description}</p>
-            <span class="price">${product.price}</span>
+            <h2 class="title">${product.title}</h2>
+            <span class="price">${product.price}.00</span>
         `;
+
         productContainer.appendChild(productCard);
 
         const imgBox = productCard.querySelector('.img-box');
+        console.log(imgBox);
         imgBox.addEventListener('click', () => {
             sessionStorage.setItem("selectProduct", JSON.stringify(product));
             window.location.href = "product-details.html";
