9806487de1ebfb1d85b2314d13a8e0dad1b5b6ed
Bachan Kubi <felixghagra@gmail.com>
2025-08-29 19:51:49 +0600

    selected pruction sections bug fix with selected colors

--- main.js
+++ main.js
@@ -1,11 +1,14 @@
 const productContainer = document.querySelector(".product-list");
 const isProductDetails = document.querySelector(".product-detail");
+const isCartPage = document.querySelector(".cart");
 
 //1- if it has product list
 if (productContainer) {
   displayProducts();
 } else if (isProductDetails) {
   displayProductDetails();
+} else if (isCartPage) {
+  displayCart();
 }
 
 //2- display product
@@ -39,29 +42,29 @@ function displayProductDetails() {
   console.log(productData);
 
   const title = document.querySelector(".title");
-  const thumbnailImage = document.querySelector(".thumbnail-img");
+  // const thumbnailImage = document.querySelector(".thumbnail-img");
   const mainImg = document.querySelector(".main-img");
   const price = document.querySelector(".price");
   const sizeOption = document.querySelector(".size-options");
   const colorOption = document.querySelector(".color-options");
   const description = document.querySelector(".description");
-  const addToCardBtn = document.querySelector(".btn-addToCard");
+  const addToCardBtn = document.querySelector("#btn-addToCard");
   const thumbnailList = document.querySelector(".thumbnail-img");
 
   // 3-default size and color if no color
   let selectedColor = productData.colors[0];
-  let selectedSize = selectedColor.size[0];
+  let selectedSize = selectedColor.sizes[0];
 
   //4- upadate product data
-  function updateProductDisplay(colorDAta) {
-    if (!colorDAta.size.includes(selectedSize)) {
-      selectedSize = colorDAta.size[0];
+  function updateProductDisplay(colorData) {
+    if (!colorData.sizes.includes(selectedSize)) {
+      selectedSize = colorData.sizes[0];
     }
     // 5- show main image when its clicked
-    mainImg.innerHTML = `<img src="${colorDAta.mainImage}" alt="main image" />`;
+    mainImg.innerHTML = `<img src="${colorData.mainImage}" alt="main image" />`;
     thumbnailList.innerHTML = "";
-    const allThumbnails = [colorDAta.mainImage].concat(
-      colorDAta.thumbnails.slice(0, 3)
+    const allThumbnails = [colorData.mainImage].concat(
+      colorData.thumbnails.slice(0, 3)
     );
     allThumbnails.forEach((thumb) => {
       const img = document.createElement("img");
@@ -75,25 +78,110 @@ function displayProductDetails() {
     productData.colors.forEach((color) => {
       const img = document.createElement("img");
       img.src = color.mainImage;
-      if(color.name===productData.colors){img.classList.add('selected')}
+      if (color.name === productData.name) {
+        img.classList.add("selected");
+      }
       colorOption.appendChild(img);
       img.addEventListener("click", () => {
-        selectedColor = color
+        selectedColor = color;
         updateProductDisplay(color);
       });
     });
     sizeOption.innerHTML = "";
-    productData.size.forEach((size)=>{
-      const btn = document.createElement('button');
+    selectedColor.sizes.forEach((size) => {
+      const btn = document.createElement("button");
       btn.textContent = size;
-      if(size===selectedSize){btn.classList.add('selected')};
+      if (size === selectedSize) {
+        btn.classList.add("selected");
+      }
       sizeOption.appendChild(btn);
-      btn.addEventListener("click", ()=>{
-        document.querySelectorAll(".size-options button").forEach((el)=>{
+      btn.addEventListener("click", () => {
+        document.querySelectorAll(".size-options button").forEach((el) => {
           el.classList.remove("selected");
-          selectedSize=size;
-        })
-      })
+          btn.classList.add("selected");
+          selectedSize = size;
+        });
+      });
     });
   }
+  title.textContent = productData.title;
+  price.textContent = productData.price;
+  description.textContent = productData.description;
+  updateProductDisplay(selectedColor);
+
+  addToCardBtn.addEventListener("click",()=>{
+    console.log(productData);
+    addToCart(productData, selectedColor, selectedSize)
+  });
 }
+
+// add button for cart fucntion 
+function addToCart(product, color, size){
+  let cart = JSON.parse(sessionStorage.getItem("cart")) || [];
+
+  const existingItems = cart.find((item)=>{
+    item.id===product.id && item.color === color.name && item.size === size
+  });
+  if(existingItems){
+    existingItems.quantity +=1;
+  }else{
+    cart.push({
+      id: product.id,
+      title: product.title,
+      color: color.name,
+      size: size,
+      quantity: 1,
+      image: color.mainImage,
+      price: product.price,
+
+    })
+  }
+  sessionStorage.setItem("cart", JSON.stringify(cart));
+}
+// 4 display cart page
+function displayCart(){
+  const cart = JSON.parse(sessionStorage.getItem("cart")) || [];
+
+  const cartItemContainer = document.querySelector(".cart-items");
+  const subTotal = document.querySelector(".subtotal");
+  const grandTotal = document.querySelector(".grand-total");
+  cartItemContainer.innerHTML = "";
+  if(cart.length === 0){
+    cartItemContainer.innerHTML = "<p>Your cart is empty</p>";
+    subTotal.textContent = "0.00";
+    grandTotal.textContent = "0.00";
+    return;
+  }
+  let subtotal = 0;
+  cart.forEach((item, index)=>{
+    const itemTotal = parseFloat(item.price.replace("$", "")) * item.quantity;
+    subtotal += itemTotal;
+    const cartItem = document.createElement("div");
+    cartItem.classList.add("cart-item");
+    cartItem.innerHTML = `
+    <div class="cart-item">
+        <div class="product">
+          <img src="${item.image}">
+          <div class="item-detail">
+            <p>${item.title}</p>
+            <div class="size-color-box">
+              <span class="size">${item.size}</span>
+              <span class="color">${item.color}</span>
+            </div>
+          </div>
+        </div>
+        <span class="price">${item.price}</span>
+        <div class="quantity">
+          <input type="number" value="${item.quantity}" data-id="${index}" min="1" />
+        </div>
+        <span class="total-price">${itemTotal}</span>
+        <button class="remove-btn" data-id="${index}"><i class="ri-delete-bin-line"></i>Remove</button>
+      </div>
+    `;
+    cartItemContainer.appendChild(cartItem);
+  });
+
+  subtotal.textContent = `${subtotal.toFixed(2)}`;
+  grandTotal.textContent = `${subtotal.toFixed(2)}`;
+}
+
